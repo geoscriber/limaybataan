d8bbb51923af3b9becc8cdee3fd45d0d533ccf26
krquinton <nscrexgst.dotr@gmail.com>
2022-06-20 23:06:53 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,255 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vicinity",
+      "name": "Vicinity",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.109283638181763,
+        "pitch": 1.0135350315209735,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4210111153499563,
+          "pitch": 1.3350883733752177,
+          "rotation": 0,
+          "target": "3-ground"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.166338354777702,
+          "pitch": 0.3020606306817353,
+          "title": "Big Daddy's Farm Resort",
+          "text": ""
+        },
+        {
+          "yaw": -1.846919183779546,
+          "pitch": 0.42643782545579434,
+          "title": "Road Lot (Block 3)",
+          "text": "There is a road lot here for Block 3 even numbered lots"
+        },
+        {
+          "yaw": -1.3864867071148659,
+          "pitch": 0.44888533282814436,
+          "title": "Road Lot (Block 3)",
+          "text": "There should be a road lot here for Block 3 odd numbered lots according to plan."
+        }
+      ]
+    },
+    {
+      "id": "1-top-view",
+      "name": "Top View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.3507584856469492,
+        "pitch": 1.5707963267948966,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.197441506155032,
+          "pitch": 0.9342563857393102,
+          "title": "Corner 4",
+          "text": "Estimated location of corner 4"
+        },
+        {
+          "yaw": 0.713410468773862,
+          "pitch": 0.814181592394787,
+          "title": "Corner 1",
+          "text": "Estimated location of corner 1"
+        },
+        {
+          "yaw": 1.5817844288478957,
+          "pitch": 0.8287216571080496,
+          "title": "Corner 2",
+          "text": "Estimated location of corner 2"
+        },
+        {
+          "yaw": -2.462450636642753,
+          "pitch": 0.9179504012973094,
+          "title": "Corner 3",
+          "text": "Estimated location of corner 3"
+        }
+      ]
+    },
+    {
+      "id": "2-top-view-2",
+      "name": "Top View 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3418818406255433,
+        "pitch": 0.9183901184322352,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.7643453083911496,
+          "pitch": 0.6108112221595228,
+          "title": "Corner 1",
+          "text": "Estimated location of Corner 1"
+        },
+        {
+          "yaw": 1.3984769026668964,
+          "pitch": 0.6753586149439688,
+          "title": "Corner 2",
+          "text": "Estimated location of Corner 2"
+        },
+        {
+          "yaw": -1.213730891801987,
+          "pitch": 1.459210114139335,
+          "title": "Corner 3",
+          "text": "Estimated location of Corner 3"
+        },
+        {
+          "yaw": -0.41719173105852114,
+          "pitch": 0.8654794687402969,
+          "title": "Corner 4",
+          "text": "Estimated location of Corner 4"
+        },
+        {
+          "yaw": -1.083769862374897,
+          "pitch": 1.083753352002562,
+          "title": "Road Lot",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-ground",
+      "name": "Ground",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.7047495096919434,
+        "pitch": 0.42175009705310273,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.372873308877118,
+          "pitch": -1.4491519128102368,
+          "rotation": 0,
+          "target": "2-top-view-2"
+        },
+        {
+          "yaw": 0.976584078367317,
+          "pitch": -0.3718606911041924,
+          "rotation": 0,
+          "target": "0-vicinity"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3880603783299357,
+          "pitch": 0.14673857860760187,
+          "title": "Corner 3",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.10500158144693117,
+          "pitch": 0.26843705666909656,
+          "title": "Corner 2",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1882643285865093,
+          "pitch": 0.2323854056527921,
+          "title": "Corner 1",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.051059274310866,
+          "pitch": 0.1043039579790932,
+          "title": "Corner 4",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Limay, Bataan",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
